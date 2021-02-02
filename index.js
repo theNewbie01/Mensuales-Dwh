9dcaa53c93aa854f51684f6d048fa9eaadd6e80a
Snneider <pinamasn@hotmail.com>
2021-02-01 23:49:58 -0500

    Funcion de borrar

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
     const _proceso = document.getElementById("proceso").value;
     const _dia = document.getElementById("dia").value;
 
-    const fila="<tr><td>"+_num+"</td><td>"+_cod+"</td><td>"+_linea+"</td><td>"+_proceso+"</td><td>"+_dia+"</td><td><select name='Status'><option value='No_Iniciado' selected>No Iniciado</option> <option value='En_progreso'>En Progreso</option> <option value='Completado'>Completado</option></select></td>"+"<td><button id='btn_borrar' onclick='borrar()'>Borrar</button></td></tr>";
+    const fila="<tr><td>"+_num+"</td><td>"+_cod+"</td><td>"+_linea+"</td><td>"+_proceso+"</td><td>"+_dia+"</td><td><select name='Status'><option value='No_Iniciado' selected>No Iniciado</option> <option value='En_progreso'>En Progreso</option> <option value='Completado'>Completado</option></select></td>"+"<td><button id='btn_borrar' onclick='borrar(this)'>Borrar</button></td></tr>";
 
     const btn = document.createElement("tr");
    	btn.innerHTML=fila;
@@ -15,3 +15,11 @@
 
 }
 
+function borrar(t)
+    {
+        const td = t.parentNode;
+        const tr = td.parentNode;
+        const table = tr.parentNode;
+        table.removeChild(tr);
+    }
+ 
