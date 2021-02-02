d52427a3ae7f60ba9dd9a5bc9b719fc4b1fdb5b3
Snneider <pinamasn@hotmail.com>
2021-02-01 23:17:00 -0500

    Ingreso de informacion a tablas

---
+++ index.js
@@ -0,0 +1,17 @@
+  function guardar(){
+   
+    const _num = document.getElementById("numero").value;
+    const _cod = document.getElementById("codigo").value;
+    const _linea = document.getElementById("linea").value;
+    const _proceso = document.getElementById("proceso").value;
+    const _dia = document.getElementById("dia").value;
+
+    const fila="<tr><td>"+_num+"</td><td>"+_cod+"</td><td>"+_linea+"</td><td>"+_proceso+"</td><td>"+_dia+"</td><td><select name='Status'><option value='No_Iniciado' selected>No Iniciado</option> <option value='En_progreso'>En Progreso</option> <option value='Completado'>Completado</option></select></td>"+"<td><button id='btn_borrar' onclick='borrar()'>Borrar</button></td></tr>";
+
+    const btn = document.createElement("tr");
+   	btn.innerHTML=fila;
+    document.getElementById("tablita").appendChild(btn);
+    
+
+}
+
