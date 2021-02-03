cd2a5556d92449f55abaa95b6f77cef01596323b
Snneider <pinamasn@hotmail.com>
2021-02-02 21:06:48 -0500

    Instalano metodo GET y POST

--- index.js
+++ index.js
@@ -2,40 +2,88 @@
 const bodyParser = require('body-parser'); //modulo para la lectura de lso archivos tipo Jason
 const express = require("express"); 
 
-
 const app = express();
 
 const { mongoose } = require('./conexion');
+const Dwh = require('./Schema')
 
-app.set('port', process.env.PORT || 3000); //se configura el puerto 3000
+ //se configura el puerto 3000
+app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'),() => {                 ///se llama al puerto
+
+                 ///se llama al puerto
+app.listen(app.get('port'),() => {
   console.log('Server on port', app.get('port'));
 });
 
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/insertar',(req,res)=>{
-  res.send(200,{insertar:[]})
-})
 
-app.get('/api/insertar:insertarId',(req,res)=>{
+  app.get('/api/insertar',(req,res)=>{
+    res.send(200,{insertar:[]})
+   })
+  
+   ///////////// Metodo Get para realizar busquedas por ID
+   app.get('/api/insertar/:insertarId',(req, res)=>{
+     let insertarId = req.params.insertarId
+     
+    Dwh.findById(insertarId,(err, insertar)=>{
+      if(err) return res.status(500).send({message:`Error al realizar la peticion:${err}`})
+      if(!insertar) return res.status(404).send({message:`El programa no existe`})
+
+      res.status(200).send({insertar})
+    })
+   })
+  /////metodo Post para insertar datos en el esquema creado
+   app.post('/api/insertar',(req,res)=>{
+      console.log('POST /api/insertar')
+      console.log(req.body)
+
+      let insertar = new Dwh() //Dwh es el nombre del esquema dado en Esquema.js
+      insertar.numero = req.body.numero
+      insertar.codigo = req.body.codigo
+      insertar.linea = req.body.linea
+      insertar.proceso = req.body.proceso
+      insertar.dia = req.body.dia
+      insertar.status = req.body.status
+
+      insertar.save((err,insertarStored)=> {
+        if(err) res.status(500).send({message:`Error al salvar en la Base de datos ${err}`})
+
+        res.status(200).send({insertar: insertarStored})
+      })
+   })
+  
+   app.put('/api/insertar/:insertarID',(req,res)=>{
+  
+   })
+  
+   app.delete('/api/insertar/insertarId',(req,res)=>{
+  
+   })
+
+///////////////////pruebas de conexion
+// app.get('/api/insertar',(req,res)=>{
+//   res.send(200,{insertar:[]})
+// })
+
+// app.get('/api/insertar:insertarId',(req,res)=>{
    
-})
+// })
 
-app.post('/api/insertar',(req,res)=>{
-  console.log(req.body)
-  res.status(200).send({message:'El producto se ha recibido'})
-})
+// app.post('/api/insertar',(req,res)=>{
+//   console.log(req.body)
+//   res.status(200).send({message:'El insertaro se ha recibido'})
+// })
 
-app.put('/api/insertar/:insertarID',(req,res)=>{
+// app.put('/api/insertar/:insertarID',(req,res)=>{
 
-})
+// })
 
-app.delete('/api/insertar/insertarId',(req,res)=>{
+// app.delete('/api/insertar/insertarId',(req,res)=>{
 
-})
+// })
 
 /////////////////creacion del Get para la Bd
 
